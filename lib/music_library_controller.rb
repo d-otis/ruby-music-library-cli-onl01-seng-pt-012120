d9df611a6de1e13a5d53c8d606936a56af1673d5
Dan Foley <dan@dan-foley.com>
2020-02-13 19:20:42 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -31,6 +31,8 @@ class MusicLibraryController
         list_artists
       when 'list genres'
         list_genres
+      when 'list artist'
+        list_songs_by_artist
       end
       input = gets.chomp
     end
